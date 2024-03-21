a110d018f4defee64544309e545869aad3aff9ad
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-21 15:57:02 -0500

    Update: Added sidebar and updated responsive styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import "./App.css";
+import AppSidebar from "./components/sidebar/AppSidebar";
 import { Outlet } from "react-router-dom";
-import CurrentPlanInfo from "./components/current-plan-info/CurrentPlanInfo";
-import AppNavbar from "./components/navbar/AppNavbar";
 
 function App() {
   return (
     <div className="app">
-      <aside className="app-sidebar">
-        <div>logo</div>
-        <AppNavbar />
-        <CurrentPlanInfo />
-      </aside>
+      <AppSidebar />
       <main className="main-content">
         <Outlet />
       </main>
