2dd72884cd3ecfcec1c5fb7cd1a4bd8a27b3180f
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-21 11:09:36 -0500

    Feat: Now title of videos folder redirect to respective videos folder

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { MOCK_DATA } from "../data/Mockdata";
 import { IVideo } from "../interfaces/IVideo";
 import { SVG_ICONS } from "../helpers/svgIcons";
+import { Link } from "react-router-dom";
 
 const DashboardPage = () => {
   const [videos, setVideos] = useState<Array<IVideo | null>>([null]);
@@ -18,33 +19,42 @@ const DashboardPage = () => {
     <div className="page-container">
       <DashboardHeader />
       <table id="customers">
-        <tr>
-          <th style={{ width: "60%" }}>
-            <div className="first-column">
-              <input type="checkbox" />
-              Nombre
-            </div>
-          </th>
-          <th className="centered">Videos</th>
-          <th className="centered">Tamaño</th>
-          <th className="centered">Última modificación</th>
-        </tr>
-        {videos.map((video, index) => (
-          <tr key={index}>
-            <td>
+        <tbody>
+          <tr>
+            <th style={{ width: "60%" }}>
               <div className="first-column">
                 <input type="checkbox" />
-                <svg width="16" height="16" fill="#3f37c9" viewBox="0 0 16 16">
-                  <path d={SVG_ICONS.FOLDER} />
-                </svg>
-                <p className="video-name">{video?.name}</p>
+                Nombre
               </div>
-            </td>
-            <td className="centered">{video?.videos_count}</td>
-            <td className="centered">{video?.size}</td>
-            <td className="centered">{video?.last_modified}</td>
+            </th>
+            <th className="centered">Videos</th>
+            <th className="centered">Tamaño</th>
+            <th className="centered">Última modificación</th>
           </tr>
-        ))}
+          {videos.map((video, index) => (
+            <tr key={index}>
+              <td>
+                <div className="first-column">
+                  <input type="checkbox" />
+                  <svg
+                    width="16"
+                    height="16"
+                    fill="#3f37c9"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d={SVG_ICONS.FOLDER} />
+                  </svg>
+                  <Link className="video-name" to={`video/${video?.id}`}>
+                    {video?.name}
+                  </Link>
+                </div>
+              </td>
+              <td className="centered">{video?.videos_count}</td>
+              <td className="centered">{video?.size}</td>
+              <td className="centered">{video?.last_modified}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
